25921f2e542be34a5a820a42335b22941404bed6
Debby2011 <deborahogunbolu@gmail.com>
2025-07-27 13:38:51 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+Name='Deborah'
+age=14
+is_student=True
+weight=46.78
+
+print(f"the data type of name is: {type(Name)}")
+print(f"the data type of name is: {type(age)}")
+print(f"the data type of name is: {type(is_student)}")
+print(f"the data type of name is: {type(weight)}")
